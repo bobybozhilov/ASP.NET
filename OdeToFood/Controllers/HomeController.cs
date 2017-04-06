ef82a35e60850d1b0bc357e61f251983512c813c
bobybozhilov <bobybozhilov@abv.bg>
2017-04-06 04:13:01 +0300

    Chapter 5 Completed

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,25 +1,62 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using OdeToFood.Models;
+using OdeToFood.Entities;
 using OdeToFood.Services;
+using OdeToFood.ViewModels;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
         private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData, IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
 
         public IActionResult Index()
         {
-            //var model = new Restaurant { Id = 1, Name = "The house of Bobo" 
-            //return new ObjectResult(model);
+            var model = new HomePageViewModel();
+            model.Restaurants = _restaurantData.GetAll();
+            model.CurrentMessage = _greeter.GetGreeting();
 
-            var model = _restaurantData.GetAll();
             return View(model);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var model = _restaurantData.Get(id);
+
+            if (model == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(RestaurantEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var newRestaurant = new Restaurant();
+                newRestaurant.Cuisine = model.Cuisine;
+                newRestaurant.Name = model.Name;
+                newRestaurant = _restaurantData.Add(newRestaurant);
+
+                return RedirectToAction("Details", new { id = newRestaurant.Id });
+            }
+            return View();
         }
     }
 }
